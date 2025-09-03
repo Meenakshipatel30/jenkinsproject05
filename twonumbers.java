c9fdc93f205e5ed8a9eb8b3cc8e5ef146b2dc630
Meenakshipatel30 <aggumeenakshi77@gmail.com>
2025-09-03 11:32:49 +0530

    first commit

---
+++ twonumbers.java
@@ -0,0 +1,13 @@
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 25;
+        int sum;
+
+        sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
